7174abd201ccf461960f7a48a823857c916d1aa3
ktracey910 <ktracey962@gmail.com>
2024-12-06 14:01:36 -0500

    updated player controller

--- modules/Player_controller_Work.v
+++ modules/Player_controller_Work.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 12/05/2024 03:16:56 PM
+// Create Date: 12/06/2024 12:07:47 PM
 // Design Name: 
 // Module Name: Player_controller_Work
 // Project Name: 
@@ -37,13 +37,17 @@ module Player_controller_Work(
     wire clean_button_up;
     wire clean_button_down;
     wire clean_is_firing;
-    wire clean_projectile;
+    wire clean_rst;
+    wire cleanbuttonup;
+    wire cleanbuttondown;
+    wire cleanisfiring;
+    wire cleanrst;
 
     // Instantiate a debouncer for each button input    
-    debouncer_1 d1(clk, button_up, clean_button_up);
-    debouncer_1 d2(clk, button_down, clean_button_down);
-    debouncer_1 d3(clk, is_firing, clean_is_firing);
-    debouncer_1 d4(clk, projectile, clean_projectile);
+    debouncer d1(clk, button_up, clean_button_up, cleanbuttonup);
+    debouncer d2(clk, button_down, clean_button_down, cleanbuttondown);
+    debouncer d3(clk, is_firing, clean_is_firing, cleanisfiring);
+    debouncer d4(clk, rst, clean_rst, cleanrst);
     // TODO: reset?!
     
     // STATE DEFINITIONS
@@ -68,40 +72,41 @@ module Player_controller_Work(
     projectile_encoded = 2'b00;
     end
     // STATE MACHINE
-    always @(posedge clk or posedge rst) begin 
-    if (rst) begin 
+    always @(negedge clk or posedge clean_rst) begin 
+    if (clean_rst) begin 
     current_state = RESET;
     projectile_encoded <= 2'b00;
     end else begin 
+    
     case (current_state)
     RESET: current_state <= LANE3;
     
     LANE1: begin 
-    if(clean_button_up) current_state <= LANE2;
+    if( cleanbuttonup) current_state <= LANE2;
     end
     
     LANE2: begin
-    if(clean_button_up) current_state <= LANE3; 
-        else if(clean_button_down) current_state <= LANE1; 
+    if(cleanbuttonup) current_state <= LANE3; 
+        else if(cleanbuttondown) current_state <= LANE1; 
         end
         
    LANE3: begin 
-   if(clean_button_up) current_state <= LANE4; 
-    else if(clean_button_down) current_state <= LANE2; 
+   if(cleanbuttonup) current_state <= LANE4; 
+    else if(cleanbuttondown) current_state <= LANE2; 
     end
     
    LANE4: begin 
-   if(clean_button_up) current_state <= LANE5; 
-    else if(clean_button_down) current_state <= LANE3; 
+   if(cleanbuttonup) current_state <= LANE5; 
+    else if(cleanbuttondown) current_state <= LANE3; 
     end
     
     LANE5: begin 
-    if(clean_button_up) current_state <= LANE6; 
-    else if(clean_button_down) current_state <= LANE4; 
+    if(cleanbuttonup) current_state <= LANE6; 
+    else if(cleanbuttondown) current_state <= LANE4; 
     end
     
     LANE6: begin
-    if(clean_button_down) current_state <= LANE5; 
+    if(cleanbuttondown) current_state <= LANE5; 
     end 
     
     default: current_state <= LANE3;
@@ -109,48 +114,12 @@ module Player_controller_Work(
     
     if(clean_is_firing) projectile_encoded[1] <= 1'b1;
     else projectile_encoded[1] = 1'b0;
-    if(clean_projectile) projectile_encoded[0] <= ~projectile_encoded[0];
+    if(projectile) projectile_encoded[0] <= 1'b1;
+    else  projectile_encoded[0] <= 1'b0;
   end
         end
     always @(*) begin 
     data_out = {rst, 1'b0, projectile_encoded, current_state};
     end 
     
-   
-    // At posedge of buttons, update our movement
-//    always @(posedge clean_button_up) begin
-//        // as long as we are not at lane, move up one lane
-//        if (~(current_state == LANE6)) begin
-//            next_state = current_state + 4'b0001; 
-//        end
-//    end
-//    always @(posedge clean_button_down) begin
-//        // as long as we are not at lane1, move down one lane
-//        if (~(current_state == LANE1)) begin //
-//            next_state = current_state - 4'b0001;
-//        end
-//    end
-//    // At posedge of rst, reset the game (asynchronous)
-//    always @(posedge rst) begin
-//        next_state = RESET;
-//        current_state = RESET;
-//        data_out = 8'b00000000;
-//        projectile_encoded = 2'b00;
-//    end
-    
-//    //  Update current state and encode output data
-//    always @(*) begin //
-//        if (current_state == RESET) begin
-//            next_state <= LANE3;
-//        end
-        
-//        // Move to the next state
-//        current_state = next_state;
-
-//        // Encode projectile data in two bits
-//        projectile_encoded[1] = clean_is_firing;
-//        projectile_encoded[0] = clean_projectile;
-//        // Encode output data: rst, (nothing/0), projectile, and lane
-//        data_out <= {rst, 1'b0, projectile_encoded, current_state}; 
-        
-        endmodule 
+endmodule
