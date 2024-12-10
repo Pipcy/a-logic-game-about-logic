5ede383211731342e88dccf5a7386b05d3e07864
ebf-hue <129566264+ebf-hue@users.noreply.github.com>
2024-12-09 20:08:47 -0500

    update player_controller.v module name

--- modules/player_controller.v
+++ modules/player_controller.v
@@ -1,26 +1,6 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/06/2024 12:07:47 PM
-// Design Name: 
-// Module Name: Player_controller_Work
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module Player_controller_Work(
+module player_controller(
  input rst,                  // reset
     input clk,                  // clock
     input button_up,            // move upwards
