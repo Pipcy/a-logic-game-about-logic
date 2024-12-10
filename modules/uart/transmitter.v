eb084ce7b0df22f405cc0ad92e52da4450a47fdb
ebf-hue <129566264+ebf-hue@users.noreply.github.com>
2024-12-09 21:41:29 -0500

    update transmitter.v

--- modules/uart/transmitter.v
+++ modules/uart/transmitter.v
@@ -6,25 +6,28 @@ module transmitter(
   input rst,
   input start,
   input [7:0] data_out,
-  output tx_out
+  output reg tx_out
 );
   reg tx, next_tx;
   reg [1:0] state, next_state;
   reg [3:0] baud_count, next_baud_count;
   reg [2:0] index, next_index;
   reg [7:0] data, next_data;
+//  reg baud_count_updated;
 
   // States
   localparam [1:0] IDLE = 2'b00, START = 2'b01, DATA = 2'b10, STOP = 2'b11;
 
   // Reset, or on posedge of clock, update our registers to the next value
-  always @(posedge clk_in or posedge reset) begin
-    if (reset) begin
+  always @(posedge clk_in or posedge rst) begin
+    if (rst) begin
       state <= IDLE;
       baud_count <= 0;
       index <= 0;
-      data <= 0;
+//      data <= 0;
+      data <= data_out;
       tx <= 1;
+//      baud_count_updated <= 0;
     end
     else begin
       state <= next_state;
@@ -32,6 +35,7 @@ module transmitter(
       index <= next_index;
       data <= next_data;
       tx <= next_tx;
+//      if (baud_count_updated) baud_count_updated <= 0;
     end
   end
 
@@ -47,7 +51,7 @@ module transmitter(
 
     case(state)
       IDLE : begin
-        tx_next = 1; // idle bit
+        next_tx = 1; // idle bit
         // If start bit is high
         if (start) begin
           next_state = START;
@@ -57,43 +61,46 @@ module transmitter(
       end
       
       START : begin
-        tx_next = 0; // start bit
+        next_tx = 0; // start bit
         // check baud
         if (clk_baud) begin
           if (baud_count == 15) begin // 16 oversampling ticks per bit
             // ready to send the data
             next_state = DATA;
-            baud_count_next = 0;
-            index_next = 0;
+            next_baud_count = 0;
+            next_index = 0;
           end
           // otherwise incremement the count
-          else baud_count_next = baud_count + 1;
+          else begin
+            next_baud_count = baud_count + 1;
+          end
         end
       end
       
       DATA : begin
         // actually send the LSB of the data
-        tx_next = data[0];
+        next_tx = data[0];
         // check baud
         if (clk_baud) begin
           if (baud_count == 15) begin
             // reset the count and shift the data
-            baud_count_next = 0;
-            data_next = data >> 1;
+            next_baud_count = 0;
+            next_data = data >> 1;
             // increment index, or stop if we're at the end
             if (index == 7)
               next_state = STOP;
             else
-              index_next = index + 1;
+              next_index = index + 1;
           end
-          else // if baud_count is not yet 15
-            baud_count_next = baud_count + 1;
+          else begin // if baud_count is not yet 15
+            next_baud_count = baud_count + 1;
+          end
         end
       end
       
       STOP : begin
         next_tx = 1;
-        if (baud_clk) begin
+        if (clk_baud) begin
           // stop bit goes for 16 ticks
           if (baud_count == 15)
             // then go back to idle
@@ -104,7 +111,7 @@ module transmitter(
     endcase
 
     // actual bit to be transmitted
-    assign tx_out = tx;
+    tx_out = tx;
   end
   
 endmodule
