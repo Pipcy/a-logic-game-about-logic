0ac40df8c6308779db35d0a576dbeab81cdb2d29
ebf-hue <129566264+ebf-hue@users.noreply.github.com>
2024-12-09 21:41:50 -0500

    update baud_clk_divider.v

--- modules/uart/baud_clk_divider.v
+++ modules/uart/baud_clk_divider.v
@@ -13,22 +13,43 @@ module baud_clk_divider(
     
     // To achieve a baud rate of 9600, we need to divide the clock by a factor of:
     // 100,000,000 / 9,600 = 10,416
-    parameter toggle_value = 10416;
+//    parameter toggle_value = 10416;
+    parameter toggle_value = 2;
 
     reg [31:0] counter;
     reg [31:0] next;
 
     always @(posedge clk_in or posedge rst) begin
-        if (rst) counter <=0;
-        else counter <= next;
-
-        if (counter == toggle_value) begin
-            next = 0;
-            clk_out = 1;
+        if (rst) begin
+            counter <= 0;
+            clk_out <= 0;
+//        end else counter <= next;
         end else begin
-            next = counter + 1;
-            clk_out = 0;
+        
+        // only stay high for one clock cycle
+//        if (clk_out == 1) clk_out = 0;
+//        else begin
+//            if (counter == toggle_value) begin
+//                next = 0;
+//                clk_out = 1;
+//                counter = 0;
+//            end else begin
+//                next = counter + 1;
+//                clk_out = 0;
+//            end
+//        end
+
+            if (counter == toggle_value - 1) begin
+//                next = 0;
+                clk_out <= ~clk_out;
+                counter <= 0;
+            end else begin
+//                next = counter + 1;
+//                clk_out = 0;
+                counter <= counter + 1;
+            end
         end
+        
     end
     
 endmodule
