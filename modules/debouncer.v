fced300378db6f82027ddb791ccb96c80a4ea269
ebf-hue <129566264+ebf-hue@users.noreply.github.com>
2024-12-10 21:38:58 -0500

    remove whitespace/comments from debouncer.v

--- modules/debouncer.v
+++ modules/debouncer.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/03/2024 07:10:02 PM
-// Design Name: 
-// Module Name: debouncer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module debouncer(
     input clk,
